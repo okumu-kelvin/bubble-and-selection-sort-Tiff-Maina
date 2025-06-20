a925e7538a5739e19b9cb760b0c7f969aae39243
Tiff-Maina <tiff.maina01@gmail.com>
2025-06-20 19:08:41 +0300

    code demonstrating Bubble sort.

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+
+    # Loop through the list multiple times
+    for i in range(n):
+        # In each pass, compare adjacent elements up to the unsorted part
+        for j in range(0, n - i - 1):
+            # If the left element is greater than the right, swap them
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+
+    # Return the sorted list
+    return unsorted_list

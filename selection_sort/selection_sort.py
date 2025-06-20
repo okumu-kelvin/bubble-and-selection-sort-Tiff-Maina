b2ed655d5ade9294a32d51fd2b80619fbedc2587
Tiff-Maina <tiff.maina01@gmail.com>
2025-06-20 20:01:06 +0300

    made changes to selection sort file

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,13 +1,10 @@
 def selection_sort(arr):
     n = len(arr)
-
-    # Traverse through all array elements
     for i in range(n):
-        # Find the index of the minimum element in remaining unsorted part
         min_index = i
         for j in range(i + 1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-
-        # Swap the found minimum element with the first unsorted element
+        # Swap the found minimum with the first unsorted element
         arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
